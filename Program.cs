29a37cb115ddc8475b46e4798511236ce69b74e6
RomanYakobchuk <romanyakobchuk28@gmail.com>
2020-11-15 14:17:00 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ads_yakobchuk_kp02_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worlds!");
+        }
+    }
+}
